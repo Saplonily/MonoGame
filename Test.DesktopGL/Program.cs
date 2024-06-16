31553e5a0af3a4c447c000b331d6d062c443f2cb
Saplonily <Saplonily@outlook.com>
2024-06-16 12:38:23 +0800

    Cleaning.3

---
+++ Test.DesktopGL/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Test.DesktopGL;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using var game = new Test.DesktopGL.TestGame();
+        game.Run();
+    }
+}
